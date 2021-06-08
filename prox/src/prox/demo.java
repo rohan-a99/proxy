e1ae2c490a60b7318ecf7f8bc5a316a09ed860e1
ROHAN KAR <rohankar1999@gmail.com>
2021-06-09 00:40:31 +0530

    forst

---
+++ prox/src/prox/demo.java
@@ -0,0 +1,15 @@
+
+package prox;
+
+public class demo {
+	
+	public static void main(String[] args) {
+		System.out.println("STARTED");
+		
+		for(int i=0;i<6;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
